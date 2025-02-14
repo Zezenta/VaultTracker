369b591a4096ed6db4d01e3b2371a0f5857e302e
José Miguel <urux11@gmail.com>
2025-02-13 20:44:09 -0500

    Add daily update to prices.json. Modify .gitignore to include package-lock

--- vaulttracker.js
+++ vaulttracker.js
@@ -5,6 +5,8 @@ const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
 const app = express();
+const CronJob = require('cron').CronJob;
+const pricesPath = path.join(__dirname, 'prices.json'); // Ruta del archivo JSON
 
 
 app.use(bodyParser.json());
@@ -12,7 +14,6 @@ app.use(session({ secret: 'mysecret', resave: false, saveUninitialized: true }))
 
 // Leer el archivo JSON con los usuarios
 const users = JSON.parse(fs.readFileSync('users.json'));
-var prices = JSON.parse(fs.readFileSync('prices.json'));
 
 // Ruta de login
 app.post('/login', (req, res) => {
@@ -64,10 +65,60 @@ app.get('/api/userdata', isAuthenticated, (req, res) => {
 });
 
 app.get('/api/prices', isAuthenticated, async (req, res) => {
-    res.json(prices)
+    try {
+        const data = fs.readFileSync(pricesPath, 'utf8');
+        const latestPrices = JSON.parse(data);
+        res.json(latestPrices);
+    } catch (error) {
+        console.error('Error al leer prices.json:', error);
+        res.status(500).send('Error al obtener precios');
+    }
 });
 
 // Iniciar el servidor
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
+
+
+// Sacar precio diario de BTC
+const job = new CronJob('5 0 * * *', async () => { //at 5 minutes past midnight
+
+    try {
+        // Obtener el precio actual
+        var price = await getPrice();
+
+        // Obtener la fecha actual en formato YYYY-MM-DD
+        const date = new Date().toISOString().split('T')[0];
+
+        // Leer el archivo JSON actual
+        let data = {};
+        if (fs.existsSync(pricesPath)) {
+            const fileContent = fs.readFileSync(pricesPath, 'utf8');
+            data = fileContent ? JSON.parse(fileContent) : {};
+        }
+
+        // Agregar el nuevo precio con la fecha actual
+        data[date] = parseFloat(price.toFixed(2));
+
+        // Guardar el archivo actualizado
+        fs.writeFileSync(pricesPath, JSON.stringify(data, null, 4));
+
+        console.log(`Precio actualizado: ${date} -> ${data[date]}`);
+    } catch (error) {
+        console.error('Error al actualizar el archivo:', error);
+    }
+
+}, null, true, 'America/Guayaquil');
+job.start();
+
+async function getPrice(){
+    const response = await fetch("https://blockchain.info/ticker");
+
+    if (!response.ok) {
+        console.log("Error al obtener precio de bitcoin")
+    }
+
+    const data = await response.json();
+    return data.USD.last;
+}
